f9cad4cd54905209dbe812eac37cbfa69cb6bf5d
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-09-29 14:32:37 +0300

    add adminlte files and test the starter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public static $data = [];
+    public function __construct()
+    {
+        self::$data['title'] = 'Store';
+    }
+
     public function index()
     {
-        return view('dashboard.index');
+        return view('dashboard',[
+            'user'  =>  'Bahaa Mohammed',
+            'title' =>  self::$data['title']
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard',[\App\Http\Controllers\DashboardController::class,'index']);
+Route::get('/dashboard',[\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard.index');
