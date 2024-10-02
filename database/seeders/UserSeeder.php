edae9da88f625bd407e8b820a62f0f3d9e273a66
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-10-02 20:18:15 +0300

    create UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'      =>  'Bahaa Mohammed',
+            'email'     =>  'bm@admin.com',
+            'password'  =>  Hash::make('password'),
+            'phone'     =>  '123456789',
+        ]);
+
+        DB::table('users')->insert([
+            'name'      =>  'Admin',
+            'email'     =>  'admin@admin.com',
+            'password'  =>  Hash::make('password'),
+            'phone'     =>  '123456788',
+        ]);
+    }
+}
